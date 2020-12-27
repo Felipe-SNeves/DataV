296e32b5bf2fac344b4ebad9951fd10f7487a150
Felipe Neves <felpslva5@gmail.com>
2020-12-27 02:41:40 -0300

    Todos os formatos prontos

--- datav.c
+++ datav.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+long int tamanho();
+void visualizao_hexa(long int);
+void visualizao_octal(long int);
+void bin(unsigned n);
+void visualizao_bin(long int);
+
 FILE * pf;
 
 int main(int argc, char ** argv)
 {
-	FILE * pf = NULL;
+	pf = NULL;
 
-	pf = fopen(*(argv+1), "rb");
+	switch(argc)
+	{
+		case 1: puts("Erro!"); exit(1);
+		case 2: if((pf = fopen(*(argv+1), "rb"))==NULL)
+			{
+				puts("Não abri!");
+				exit(1);
+			}
+	}
 
 	visualizao_hexa(tamanho());
 
@@ -22,6 +36,7 @@ long int tamanho()
 	aux = ftell(pf);
 	rewind(pf);
 
+
 	return aux;
 }
 
@@ -29,11 +44,60 @@ void visualizao_hexa(long int tam)
 {
 	unsigned char * buffer = (unsigned char *) malloc(tam);
 	unsigned char * ptr = buffer;
+	int i = 0;
 
 	fread(buffer,tam,1,pf);
 
 	while(*ptr)
-		printf("%u", *ptr++);
-
+	{
+		if(!(i%16))
+			printf("\n0x%.8X\t", i);
+		printf("%.2X", *ptr++);
+		if(i%2)
+			putchar(' ');
+		i++;
+	}
+	putchar('\n');
 	free(buffer);
+
+}
+
+void visualizao_octal(long int tam)
+{
+	unsigned char * buffer = (unsigned char *) malloc(tam);
+	unsigned char * ptr = buffer;
+
+	fread(buffer,tam,1,pf);
+
+	while(*ptr)
+		printf("%.3o ", *ptr++);
+
+	putchar('\n');
+	free(buffer);
+
+}
+
+void visualizao_bin(long int tam)
+{
+	unsigned char * buffer = (unsigned char *) malloc(tam);
+	unsigned char * ptr = buffer;
+
+	fread(buffer,tam,1,pf);
+
+	while(*ptr)
+	{
+		bin(*ptr++);
+		putchar(' ');
+	}
+
+	putchar('\n');
+	free(buffer);
+
+}
+
+void bin(unsigned n)
+{
+    unsigned i;
+    for(i = 1<< 7; i > 0; i = i / 2)
+        (n & i) ? printf("1") : printf("0");
 }
